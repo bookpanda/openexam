7519949a27e82ef5c44f22950af73f361375972e
Idhibhat Pankam <marginpankam@gmail.com>
2025-09-15 00:55:12 +0700

    1

--- user/src/services/auth.rs
+++ user/src/services/auth.rs
@@ -1,4 +1,9 @@
-use oauth2::{CsrfToken, EndpointNotSet, EndpointSet, Scope};
+use oauth2::basic::BasicTokenType;
+use oauth2::{
+    AuthorizationCode, CsrfToken, EndpointNotSet, EndpointSet, ExtraTokenFields, Scope,
+    StandardTokenResponse, TokenResponse,
+};
+use serde::Deserialize;
 use tonic::{Request, Response, Status};
 
 use crate::config::config::OAuthConfig;
@@ -6,17 +11,25 @@ use crate::proto::auth::{
     GetGoogleLoginUrlReply, GetGoogleLoginUrlRequest, LoginReply, LoginRequest,
 };
 use crate::repositories::user::UserRepo;
+use jsonwebtoken::{DecodingKey, Validation, decode};
 use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl, basic::BasicClient};
+
+#[derive(Debug, Deserialize)]
+struct GoogleExtraTokenFields {
+    id_token: String,
+}
+type GoogleTokenResponse = StandardTokenResponse<GoogleExtraTokenFields, BasicTokenType>;
+impl ExtraTokenFields for GoogleExtraTokenFields {}
 
 #[derive(Debug)]
 pub struct AuthService {
-    pub auth_repo: UserRepo,
+    pub user_repo: UserRepo,
     oauth_client:
         BasicClient<EndpointSet, EndpointNotSet, EndpointNotSet, EndpointNotSet, EndpointSet>,
 }
 
 impl AuthService {
-    pub fn new(auth_repo: UserRepo, oauth_config: OAuthConfig) -> anyhow::Result<Self> {
+    pub fn new(user_repo: UserRepo, oauth_config: OAuthConfig) -> anyhow::Result<Self> {
         let oauth_client = oauth2::basic::BasicClient::new(ClientId::new(oauth_config.client_id))
             .set_client_secret(ClientSecret::new(oauth_config.client_secret))
             .set_auth_uri(AuthUrl::new(oauth_config.auth_url).unwrap())
@@ -24,7 +37,7 @@ impl AuthService {
             .set_redirect_uri(RedirectUrl::new(oauth_config.redirect_url).unwrap());
 
         Ok(Self {
-            auth_repo,
+            user_repo,
             oauth_client,
         })
     }
@@ -43,6 +56,28 @@ impl AuthService {
         Ok(Response::new(GetGoogleLoginUrlReply {
             url: auth_url.to_string(),
         }))
+    }
+
+    async fn exchange_google_code(&self, code: String) -> Result<String, anyhow::Error> {
+        let code = AuthorizationCode::new(code);
+
+        let token_response: GoogleTokenResponse = self
+            .oauth_client
+            .exchange_code(code)
+            .request_async(oauth2::reqwest::async_http_client)
+            .await?;
+
+        let access_token = token_response.access_token().secret().clone();
+
+        let id_token = token_response.extra_fields().id_token.clone();
+
+        let token_data = decode::<GoogleClaims>(
+            &id_token,
+            &DecodingKey::from_secret("YOUR_GOOGLE_CLIENT_SECRET".as_ref()),
+            &Validation::default(),
+        )?;
+
+        Ok(access_token.to_string())
     }
 
     pub async fn login(
