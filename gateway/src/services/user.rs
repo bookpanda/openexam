c05b335d43e8b89fa7a86c9a08c38e6a4bcc5632
Idhibhat Pankam <marginpankam@gmail.com>
2025-09-15 15:59:14 +0700

    1

--- gateway/src/services/user.rs
+++ gateway/src/services/user.rs
@@ -1,58 +1,30 @@
-use crate::models::user::User;
-use crate::repositories::user::UserRepo;
-use crate::services::response::ServiceResponse;
+use tonic::transport::Channel;
+
+use crate::{
+    proto::auth::{GetGoogleLoginUrlRequest, LoginRequest, auth_client::AuthClient},
+    services::response::ServiceResponse,
+};
 
 #[derive(Debug)]
 pub struct UserService {
-    user_repo: UserRepo,
+    user_client: AuthClient<Channel>,
 }
 
 impl UserService {
-    pub fn new(user_repo: UserRepo) -> Self {
-        Self { user_repo }
+    pub fn new(user_client: AuthClient<Channel>) -> Self {
+        Self { user_client }
     }
 
-    pub async fn get_all(&self) -> ServiceResponse<Vec<User>> {
-        match self.user_repo.get_all().await {
-            Ok(users) => ServiceResponse::ok(users),
+    pub async fn get_google_login_url(&self) -> ServiceResponse<String> {
+        let request = GetGoogleLoginUrlRequest {};
+        match self.user_client.get_google_login_url(request).await {
+            Ok(response) => ServiceResponse::ok(response.into_inner().url),
             Err(_) => ServiceResponse::internal_error("Database error"),
         }
     }
-    pub async fn get_one(&self, id: i32) -> ServiceResponse<User> {
-        match self.user_repo.get_one(id).await {
-            Ok(Some(user)) => ServiceResponse::ok(user),
-            Ok(None) => ServiceResponse::not_found("User not found"),
-            Err(_) => ServiceResponse::internal_error("Database error"),
-        }
-    }
-
-    pub async fn find_by_email(&self, email: String) -> ServiceResponse<User> {
-        match self.user_repo.find_by_email(email).await {
-            Ok(Some(user)) => ServiceResponse::ok(user),
-            Ok(None) => ServiceResponse::not_found("User not found"),
-            Err(_) => ServiceResponse::internal_error("Database error"),
-        }
-    }
-
-    pub async fn create(&self, name: String) -> ServiceResponse<User> {
-        match self.user_repo.create(name).await {
-            Ok(user) => ServiceResponse::created(user),
-            Err(_) => ServiceResponse::internal_error("Database error"),
-        }
-    }
-
-    pub async fn update(&self, id: i32, name: String) -> ServiceResponse<User> {
-        match self.user_repo.update(id, name).await {
-            Ok(Some(user)) => ServiceResponse::ok(user),
-            Ok(None) => ServiceResponse::not_found("User not found"),
-            Err(_) => ServiceResponse::internal_error("Database error"),
-        }
-    }
-
-    pub async fn delete(&self, id: i32) -> ServiceResponse<String> {
-        match self.user_repo.delete(id).await {
-            Ok(true) => ServiceResponse::ok("User deleted".to_string()),
-            Ok(false) => ServiceResponse::not_found("User not found"),
+    pub async fn login(&self, request: LoginRequest) -> ServiceResponse<String> {
+        match self.user_client.login(request).await {
+            Ok(response) => ServiceResponse::ok(response.into_inner().message),
             Err(_) => ServiceResponse::internal_error("Database error"),
         }
     }
