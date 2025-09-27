a4c96bff3f1d8379632eb0dec0fbb9ca124d1d4c
tnsp <wavet888@gmail.com>
2025-09-28 00:37:23 +0700

    add share,unshare rest api

--- cheatsheet/internal/app/router.go
+++ cheatsheet/internal/app/router.go
@@ -6,9 +6,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func SetupRoutes(app *fiber.App, fh *handler.FileHandler) {
+func SetupRoutes(app *fiber.App, fh *handler.FileHandler, sh *handler.ShareHandler) {
 	app.Post("/upload", fh.Upload)
 	app.Get("/files", fh.Download)                // GET /files?key=...
 	app.Delete("/files", fh.Remove)               // DELETE /files?key=...
 	app.Get("/files/presign", fh.GetPresignedURL) // GET /files/presign?key=...
+
+	// share/unshare
+	app.Post("/share", sh.Share)
+	app.Post("/unshare", sh.Unshare)
 }
