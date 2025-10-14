8cf0ca8af9ce5c7b1d1fadb799844e961561ff12
rsosisnsusjs <rutchaputch47@gmail.com>
2025-10-14 11:47:56 +0700

    feat: new signup page catchphrase

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -6,10 +6,15 @@ import Shuffle from "@/components/Shuffle"
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-[100svh] flex-col items-center justify-center gap-10 p-6 md:p-10">
-
-      <h1 className="text-4xl font-extrabold text-center">
-        Open Exam
-      </h1>
+      
+      <div className="text-center space-y-2">
+        <h1 className="text-4xl font-extrabold tracking-tight">
+          OPEN EXAM
+        </h1>
+        <p className="text-muted-foreground text-lg max-w-md mx-auto">
+          Sigma students do not study — We generate.
+        </p>
+      </div>
 
       <div className="flex w-full max-w-sm flex-col rounded-2xl">
         <LoginForm />
